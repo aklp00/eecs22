2f6d875edf7d81d26bb46c6baaa53cf64952bd8e
Akhil Pappu <pappuakhil0@gmail.com>
2021-01-06 13:36:37 -0800

    new commit

---
+++ hw1/helloQV.c
@@ -0,0 +1,19 @@
+/********************************************/
+/* helloQV.c: our first C submission        */
+/* authors: QV Dang, <your name here>       */
+/*                                          */
+/* modifications:                           */
+/* 2021/01/06 Akhil Fixed error messages    */
+/* 2020/08/03   QV  Updated version         */
+/* 2018/09/25   QV  Initial version         */
+/********************************************/
+
+#include <stdio.h> 
+// function that prints out a hello
+int main(void) 
+{
+    printf("Hello ANSI standard C99!\n");
+    return 0;
+}
+
+/* EOF */
