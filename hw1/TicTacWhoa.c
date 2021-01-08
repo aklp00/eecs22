95a3d717c1c61b580428992992739c97672eec32
PAPPU <apappu@crystalcove.eecs.uci.edu>
2021-01-08 12:33:39 -0800

    First commit

---
+++ hw1/TicTacWhoa.c
@@ -0,0 +1,15 @@
+
+int hvh() { 
+	int board[3][3];
+ 
+
+
+
+int main(void) { 
+	//the menu 
+	printf("Welcome to Secure-Tic-Tac-Whoa!\n");
+	printf("1. Start New Game (Human v. Human)\n");
+	printf("2. Start New Game (Human v. AI)\n");
+	printf("3. Game Settings\n");
+	printf("4. Exit Game\n");
+
